b1fa79cab3204e1e27c46f510cea1738a97be1d1
NVdty <nadity6@gmail.com>
2024-10-17 13:13:50 +0700

    update tampilan blog

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,23 +2,6 @@
 
     <x-slot:title>{{ $title }}</x-slot:title>
 
-    {{-- <article class="py-8 max-w-screen-md ">
-        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post ['title'] }}</h2>
-            <div">
-                By
-                <a href="/author/{{ $post->author->username }}" class="hover:underline ">{{ $post->author->name }}</a>
-                in
-                <a href="/categories/{{$post->category->slug}}>{{$post->category->name}}</a> |
-                {{ $post->created_at->format('d F Y') }}
-            </div>
-            
-            <p class="my-4 font-light">
-            {{ $post ['body'] }}
-            </p>
-            <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to Post</a>
-    </article>
-     --}}
-
     <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
         <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
             <article
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,22 +8,6 @@
             <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
 
                 @foreach ($posts as $post)
-        {{-- <article class="py-8 max-w-screen-md border-b border-gray-300">
-            <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
-                <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
-            </a>
-            <div>
-                By
-                <a href="/author/{{ $post->author->username }}" class="hover:underline ">{{ $post->author->name }}</a>
-                in
-                <a href="/categories/{{$post->category->slug}}" class="hover:underline"  class="text-base text-gray-500"> {{$post->category->name}}</a> |
-                {{ $post->created_at->format('d F Y') }}
-            </div>
-            <p class="my-4 font-light">
-                {{ Str::limit($post['body'], 100) }}
-            </p>
-            <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
-        </article> --}}
 
                     <article
                         class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
